7ec3725bb0c9f61ae8ae68d61c07d5d6b7389c98
Gaurav Patil <gp949958@gmail.com>
2025-10-20 19:10:55 +0530

    added feedback model

---
+++ server/models/Feedback.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const feedbackSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    mobile: { type: String, required: true },
+    email: { type: String, required: true },
+    message: { type: String, required: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    role: { type: String },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Feedback = mongoose.model('Feedback', feedbackSchema);
+
+export default Feedback;
