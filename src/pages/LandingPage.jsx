9e38a8e9c4a437d00cd40049338b3f313a4dd4f9
Gaurav Patil <gp949958@gmail.com>
2026-03-27 01:51:13 +0530

    responsive fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -681,7 +681,7 @@ export const LandingPage = () => {
                                         className="flex items-start gap-2"
                                     >
                                         <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-[#3b82f6] shrink-0 mt-0.5 sm:w-4"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
-                                        <span className={`text-[10px] sm:text-sm leading-relaxed font-medium transition-colors ${isDark ? 'text-black hover:text-[#47C4B7]' : 'text-gray-400 hover:text-white'}`}>+91 7875335539</span>
+                                        <a href="tel:+917875335539" className={`text-[10px] sm:text-sm leading-relaxed font-medium transition-colors ${isDark ? 'text-black hover:text-[#47C4B7]' : 'text-gray-400 hover:text-white'}`}>+91 7875335539</a>
                                     </motion.li>
                                     <motion.li 
                                         whileHover={{ x: 5 }}
@@ -690,7 +690,7 @@ export const LandingPage = () => {
                                         className="flex items-start gap-2"
                                     >
                                         <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-[#3b82f6] shrink-0 mt-0.5 sm:w-4"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
-                                        <span className={`text-[10px] sm:text-sm leading-relaxed font-medium transition-colors ${isDark ? 'text-black hover:text-[#47C4B7]' : 'text-gray-400 hover:text-white'}`}>gauravpatil@gmail.com</span>
+                                        <a href="mailto:gauravpatil@gmail.com" className={`text-[10px] sm:text-sm leading-relaxed font-medium transition-colors ${isDark ? 'text-black hover:text-[#47C4B7]' : 'text-gray-400 hover:text-white'}`}>gauravpatil@gmail.com</a>
                                     </motion.li>
                                 </ul>
                             </motion.div>
